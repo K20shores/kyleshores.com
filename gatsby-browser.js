8ad0e2969ec2866615f047ccdeaad7e3abb45af1
Kyle Shores <kyle.shores44@gmail.com>
2022-05-12 16:00:41 -0500

    deleting old stuff to start a new site

--- gatsby-browser.js
+++
@@ -1,8 +0,0 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/browser-apis/
- */
-
-import "./src/global.scss"
-import "./node_modules/react-datepicker/src/stylesheets/datepicker.scss"
