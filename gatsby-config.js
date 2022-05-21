12b25fbbd72eea5c9299ee36b7c588f01e5d7679
Kyle Shores <kyle.shores44@gmail.com>
2022-05-21 00:30:02 -0500

    adding google tracking ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,12 +55,12 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-sass`,
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     trackingId: `ADD YOUR TRACKING ID HERE`,
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-179020176-1`,
+      },
+    },
     {
       resolve: `gatsby-plugin-feed`,
       options: {
