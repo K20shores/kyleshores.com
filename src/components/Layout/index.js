52da83df73cda0c95ea93545bb300c0f4a9736d9
Kyle Shores <kyle.shores44@gmail.com>
2023-02-08 08:20:56 -0600

    changing import case

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,3 +1,3 @@
 export { default } from "./layout"
-export { default as Footer } from "./footer"
-export { default as Header } from "./header"
+export { default as Footer } from "./Footer"
+export { default as Header } from "./Header"
