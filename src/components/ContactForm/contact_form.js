10f69a6976f7435665235537d2788710f8fd5e21
Kyle Shores <kyle.shores44@gmail.com>
2023-09-14 22:54:04 -0500

    trying to get it to work

--- src/components/ContactForm/contact_form.js
+++ src/components/ContactForm/contact_form.js
@@ -8,7 +8,8 @@ function ContactForm() {
   return (
     <ContactFormWrapper>
       <h2>Contact me!</h2>
-      <form name="contact" method="POST" data-netlify="true">
+      <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+        <input type="hidden" name="form-name" value="contact" />
         <p>
           <label>
             Your Name <input type="text" name="name" />
